d87b03f09771bee3248f5be3bc4d4421be243ae4
JinduOkoli <46223684+JinduOkoli@users.noreply.github.com>
2021-10-14 16:37:35 -0400

    Update cmdo.py

--- lib/netconify/cmdo.py
+++ lib/netconify/cmdo.py
@@ -299,7 +299,7 @@ class netconifyCmdo(object):
         if self.on_notify is not None:
             self.on_notify(self, event, message)
         elif self.on_notify is not False:
-            print "{0}:{1}".format(event, message)
+            print("{0}:{1}".format(event, message))
 
     # -------------------------------------------------------------------------
     # LOGIN/LOGOUT
